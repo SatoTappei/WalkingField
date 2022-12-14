ffd1c45dde63e6fc1679804e1663d9b0c8d02e1e
SatouTappei <stutpi.signup@gmail.com>
2022-12-14 15:06:08 +0900

    経過ターンの表示を実装

---
+++ Assets/MVP/MVPTest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+/// <summary>
+/// MVPパターンでUIを実装するためにテスト用の値を保持する
+/// 最終的にMergeするときは値を良い感じに移す
+/// </summary>
+public class MVPTest : MonoBehaviour
+{
+    // 経過ターン、決定ボタンが押されるたびに+1される
+    ReactiveProperty<int> _progTurn = new ReactiveProperty<int>(0);
+
+    public IReadOnlyReactiveProperty<int> ProgTurn => _progTurn;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // テスト、キーが押されたら経過ターンを増やす
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _progTurn.Value++;
+            Debug.Log("次のターンへ");
+        }
+    }
+}
---
+++ Assets/MVP/TurnPresenter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+/// <summary>
+/// 経過ターンの表示を行うPresenter
+/// </summary>
+public class TurnPresenter : MonoBehaviour
+{
+    [SerializeField] TurnView _view;
+    [SerializeField] MVPTest _model;
+
+    void Start()
+    {
+        _model.ProgTurn.Subscribe(i => _view.Set(i.ToString())).AddTo(this);
+    }
+
+    void Update()
+    {
+        
+    }
+}
